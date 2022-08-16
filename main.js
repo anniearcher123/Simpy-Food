e235db80cdc49910877f35eb92e021f775bd0967
craw11ng <99703195+craw11ng@users.noreply.github.com>
2022-08-16 19:00:52 -0400

    Filters
    Added filter buttons to HTML, added filter functions to JS.

--- main.js
+++ main.js
@@ -27,3 +27,39 @@ function resultList(){
         console.log(data)
     })
 }
+
+// Filter recipes on homescreen
+filterObjects("all");
+
+function filterObjects(a){
+    x = document.getElementsByClassName("card");
+    if(a == "all") a = "";
+    for (i = 0; i < x.length; i++) {
+        removeClass(x[i], "show");
+        if(x[i].className.indexOf(a) > -1) addClass(x[i], "show")
+    }
+}
+
+function addClass(b, c){
+    let i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++){
+        if (arr1.indexOf(arr2[i]) == -1){
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function removeClass(b, c){
+    let i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++){
+        while (arr1.indexOf(arr2[i]) > -1){
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+// Filter recipes on homescreen
