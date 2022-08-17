6be7d73d7b9fb7271e0941e890be1b7fcab1e857
aronCampos <aron.campos400@gmail.com>
2022-08-16 17:31:48 -0700

    removed unneeded comments

--- main.js
+++ main.js
@@ -2,13 +2,7 @@
 //API key 7be06ed1dc724fc38a11ef37e6e88fbe
 //Format add => &apiKey=7be06ed1dc724fc38a11ef37e6e88fbe at the end of url
 // Example: https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples&apiKey=7be06ed1dc724fc38a11ef37e6e88fbe
-// comment
-//Test fetching recipe by ingredient
-// async function start() {
-//     const response = await fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples&apiKey=7be06ed1dc724fc38a11ef37e6e88fbe")
-//     const data = await response.json()
-//     console.log(data)    
-// }
+
 const recipes = document.getElementById('card-container')
 //start()
 
@@ -25,21 +19,38 @@ function resultList(){
     .then(response => response.json())
     .then(data => {
          console.log(data)
-         //combinedIngredients(data)
-         //let allIngredients = data.missedIngredients.concat(data.usedIngredients)
          ingredientList(data)
-         //function combine ingredients
      })
  }
-//global variable = line 45 .map function
 
-
-// function combinedIngredients(data){
-//     //loop thru arrays then concat 
-//     let allIngredients = data.missedIngredients.concat(data.usedIngredients)
-//     return allIngredients
-// }
-
+//Function that will use result of search, loop through array and pull out information
+function ingredientList(ingredient){
+    //console.log(ingredient)
+    // for(let i = 0; i < ingredient.missedIngredients.length; i++){
+        
+    // }
+    document.querySelector('.card-container').innerHTML =`
+    ${ingredient.map(function(food) {
+        
+        let html = ""
+        food.missedIngredients.forEach(function(missed){
+            html += `<li>${missed.original}</li>`
+        })
+        food.usedIngredients.forEach(function(used){
+            html += `<li>${used.original}</li>`
+        })
+        console.log(html)
+        return `<div class="card">
+        <div class="card-body">
+            <img src="${food.image}" alt="" class="card-image"/>
+            <h2 class="card-title">${food.title}</h2>
+            <ol>${html}</ol>
+        </div>
+        <button class="card-button">View recipe</button>
+    </div>`
+    }).join('')}
+    `
+}
 
 // Filter recipes on homescreen
 filterObjects("all");
@@ -78,31 +89,4 @@ function removeClass(b, c){
 // Filter recipes on homescreen
 
 
-//Function that will use result of search, loop through array and pull out information
-function ingredientList(ingredient){
-    //console.log(ingredient)
-    // for(let i = 0; i < ingredient.missedIngredients.length; i++){
-        
-    // }
-    document.querySelector('.card-container').innerHTML =`
-    ${ingredient.map(function(food) {
-        
-        let html = ""
-        food.missedIngredients.forEach(function(missed){
-            html += `<li>${missed.original}</li>`
-        })
-        food.usedIngredients.forEach(function(used){
-            html += `<li>${used.original}</li>`
-        })
-        console.log(html)
-        return `<div class="card">
-        <div class="card-body">
-            <img src="${food.image}" alt="" class="card-image"/>
-            <h2 class="card-title">${food.title}</h2>
-            <ol>${html}</ol>
-        </div>
-        <button class="card-button">View recipe</button>
-    </div>`
-    }).join('')}
-    `
-}
+
