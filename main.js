3c3a3f13d1563272508a134b34b14d772b313377
craw11ng <99703195+craw11ng@users.noreply.github.com>
2022-08-18 21:23:48 -0400

    Working on Modal

--- main.js
+++ main.js
@@ -77,3 +77,14 @@ function ingredientList(ingredient){
     `
 }
 
+let modalBtn = document.querySelector('.modal-btn')
+let modalBg = document.querySelector('.modal-bg')
+let modalClose = document.querySelector('.modal-close')
+
+modalBtn.addEventListener('click',function(){
+    modalBg.classList.add('bg-active');
+})
+
+modalClose.addEventListener('click', function(){
+    modalBg.classList.remove('bg-active');
+})
